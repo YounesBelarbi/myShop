f6541713538161438b970d34ea11f9f58d8acfc9
YounesBelarbi <ybelarbi.dev@gmail.com>
2021-08-11 16:10:41 +0200

    build: fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Nelmio\Alice\Loader\NativeLoader;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $em)
+    {
+        $loader = new NativeLoader();
+        $entities = $loader->loadFile(__DIR__ . '/fixtures.yaml')->getObjects();
+
+        foreach ($entities as $entity) {
+            $em->persist($entity);
+        };
+
+        $em->flush();
+    }
+}
