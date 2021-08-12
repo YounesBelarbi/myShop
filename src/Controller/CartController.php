7ffd72e38c84a97f007269b1d1c9c805e53fc004
YounesBelarbi <ybelarbi.dev@gmail.com>
2021-08-12 17:40:31 +0200

    feat: route to empty the cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
@@ -14,5 +15,14 @@ class CartController extends AbstractController
     public function cartShow(): Response
     {
         return $this->render('cart/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/cart/empty", name="cart_empty")
+     */
+    public function cartEmpty(SessionInterface $session): Response
+    {
+        $session->remove('cart');
+        return $this->redirectToRoute('cart_show');
     }
 }
